45d12173b134518ff9cf4f8961563bf3946bfe59
Ian Hakanson <gabianhak@gmail.com>
2025-04-01 02:40:33 -0300

    feat: add chessboard initializer

--- chessboard.go
+++ chessboard.go
@@ -1,7 +1,9 @@
-package chessboard
+package main
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -19,22 +21,58 @@ const (
 	BPAWN
 )
 
+var charToPiece = map[byte]int{
+	'K': WKING,
+	'Q': WQUEEN,
+	'R': WROOK,
+	'B': WBISHOP,
+	'N': WKNIGHT,
+	'P': WPAWN,
+	'k': BKING,
+	'q': BQUEEN,
+	'r': BROOK,
+	'b': BBISHOP,
+	'n': BKNIGHT,
+	'p': BPAWN,
+}
+
+var pieceToChar = map[int]byte{
+	WKING:   'K',
+	WQUEEN:  'Q',
+	WROOK:   'R',
+	WBISHOP: 'B',
+	WKNIGHT: 'K',
+	WPAWN:   'P',
+	BKING:   'k',
+	BQUEEN:  'q',
+	BROOK:   'r',
+	BBISHOP: 'b',
+	BKNIGHT: 'k',
+	BPAWN:   'p',
+}
+
+const initialFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
 type pair struct {
 	col int8
 	row int8
 }
 
 type Chessgame struct {
-	Moves       []string
-	Variant     string
+	Moves []string
+	// Variant     string
 	WhiteToMove bool
-	BoardState  []uint64
+	BoardState  [13]uint64
+
+	EnPassantSquare pair
 
 	BlackKingCastle  bool
 	BlackQueenCastle bool
 	WhiteKingCastle  bool
 	WhiteQueenCastle bool
-	EnPassantSquare  pair
+
+	HalfmoveClock   int
+	FullmoveCounter int
 }
 
 func addPair(a, b pair) pair {
@@ -44,7 +82,84 @@ func addPair(a, b pair) pair {
 	}
 }
 
+func pairToInt(a pair) int {
+	return int(8*a.row + a.col)
+}
+
 func CreateChessgame() Chessgame {
 	chessgame := Chessgame{}
+	row, col := int8(0), int8(0)
+	FENparts := strings.Split(initialFEN, " ")
+
+	// position parsing
+	for i := 0; i < len(FENparts[0]); i++ {
+		c := FENparts[0][i]
+		if c > '0' && c < '9' {
+			col += int8(c - '0')
+			continue
+		}
+
+		if c == '/' {
+			row++
+			col = 0
+			continue
+		}
+		piece := charToPiece[c]
+		position := pairToInt(pair{col, row})
+		chessgame.BoardState[piece] |= 1 << position
+		col++
+
+	}
+
+	// side to move parsing
+	chessgame.WhiteToMove = FENparts[1] == "w"
+
+	// castling rights parsing
+	for _, c := range FENparts[2] {
+		// "KQkq"
+		if c == 'K' {
+			chessgame.WhiteKingCastle = true
+		}
+		if c == 'Q' {
+			chessgame.WhiteQueenCastle = true
+		}
+		if c == 'k' {
+			chessgame.BlackKingCastle = true
+		}
+		if c == 'q' {
+			chessgame.BlackQueenCastle = true
+		}
+	}
+
+	// en passant
+	// TODO: prettify this piece of shit
+	if FENparts[3] != "-" {
+		chessgame.EnPassantSquare.row = int8(FENparts[3][1] - '0')
+		chessgame.EnPassantSquare.col = int8(FENparts[3][2] - '`')
+	}
+
+	// half
+	// TODO: handle a little more gracefully the error
+	halfMove, err := strconv.ParseInt(FENparts[4], 10, 0)
+	if err != nil {
+		panic(err)
+	}
+	chessgame.HalfmoveClock = int(halfMove)
+
+	// full
+	fullMove, err := strconv.ParseInt(FENparts[5], 10, 0)
+	if err != nil {
+		panic(err)
+	}
+	chessgame.FullmoveCounter = int(fullMove)
+
 	return chessgame
+}
+
+func (c *Chessgame) makeMove(move string) bool {
+	return true
+}
+
+func main() {
+	CreateChessgame()
 }
