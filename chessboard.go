24e618570ea40b5094bfb2d7709e440c2792dfb9
Ian Gabriel Hakanson <gabianhak@gmail.com>
2025-04-26 03:22:17 -0300

    feat: checkmovelegality, add castling edge case

--- chessboard.go
+++ chessboard.go
@@ -51,6 +51,11 @@ var pieceToChar = map[int]byte{
 	BPAWN:   'p',
 }
 
+const (
+	BLACK = false
+	WHITE = true
+)
+
 const initialFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 type pair struct {
@@ -79,6 +84,11 @@ type Chessgame struct {
 
 	HalfmoveClock   int
 	FullmoveCounter int
+}
+
+// WARNING: FUNCTION VERY PERIGLOSA. Use at your own risk or smth
+func sq(s string) pair {
+	return pair{col: int8(s[0] - 'a'), row: int8(s[1] - '0')}
 }
 
 func addPair(a, b pair) pair {
@@ -166,11 +176,144 @@ func inBounds(p pair) bool {
 	return p.col >= 0 && p.col < 8 && p.row >= 0 && p.row < 8
 }
 
-func (c *Chessgame) CheckMoveLegality(Move) bool {
+func (c *Chessgame) erasePiece(s pair) {
+	// NOTE: check if range is a copy or a reference
+	piecePosition := pairToInt(s)
+	bitAux := ^uint64(0) ^ uint64(1<<piecePosition)
+
+	for i := 0; i < len(c.BoardState); i++ {
+		c.BoardState[i] &= bitAux
+	}
+}
+
+func (c *Chessgame) putPiece(s pair, piece int) {
+	c.erasePiece(s)
+	piecePosition := pairToInt(s)
+	bitAux := uint64(1 << piecePosition)
+	c.BoardState[piece] |= bitAux
+}
+
+func (c *Chessgame) SquareIsThreattenned(color bool, p pair) bool {
+	return false
+}
+
+func (c *Chessgame) CheckMoveLegality(move Move) bool {
+	// check inbounds
+	if !(inBounds(move.from) || inBounds(move.from)) {
+		return false
+	}
+	fromPiece := c.getPiece(move.from)
+	//  check if frompiece is not empty
+	if fromPiece == 0 {
+		return false
+	}
+	toPiece := c.getPiece(move.to)
+
+	// toPiece is not of the same color
+	if toPiece != 0 && isWhite(toPiece) == isWhite(fromPiece) {
+		return false
+	}
+
+	// if is the correct turn to play
+	if isWhite(fromPiece) != c.WhiteToMove {
+		return false
+	}
+
+	// lists of changes
+	//
+
+	var squaresToErase []pair     //
+	var squaresToPutPieces []pair // squares where there are pieces
+	var piecesPerSquare []int     //
+
+	// make move
+	switch {
+	// castling
+	case fromPiece == BKING:
+		if (move.from == pair{col: 4, row: 7}) && (move.to == pair{col: 2, row: 7}) {
+			if !c.BlackQueenCastle {
+				return false
+			}
+			if c.SquareIsThreattenned(WHITE, sq("e8")) || c.SquareIsThreattenned(WHITE, sq("d8")) || c.SquareIsThreattenned(WHITE, sq("c8")) {
+				return false
+			}
+			if c.getPiece(sq("d8")) != 0 || c.getPiece(sq("c8")) != 0 || c.getPiece(sq("b8")) != 0 {
+				return false
+			}
+
+			// WARNING: I THINK THERE IS NO OTHER EDGE CASE.
+			return true
+		}
+		if (move.from == pair{col: 4, row: 7}) && (move.to == pair{col: 6, row: 7}) {
+			if !c.BlackKingCastle {
+				return false
+			}
+			if c.SquareIsThreattenned(WHITE, sq("e8")) || c.SquareIsThreattenned(WHITE, sq("f8")) || c.SquareIsThreattenned(WHITE, sq("g8")) {
+				return false
+			}
+			if c.getPiece(sq("f8")) != 0 || c.getPiece(sq("g8")) != 0 {
+				return false
+			}
+			// WARNING: I THINK THERE IS NO OTHER EDGE CASE
+			return true
+		}
+	case fromPiece == WKING:
+		if (move.from == pair{col: 4, row: 0}) && (move.to == pair{col: 2, row: 0}) {
+			if !c.WhiteQueenCastle {
+				return false
+			}
+			if c.SquareIsThreattenned(BLACK, sq("e1")) || c.SquareIsThreattenned(BLACK, sq("d1")) || c.SquareIsThreattenned(BLACK, sq("c1")) {
+				return false
+			}
+			if c.getPiece(sq("d1")) != 0 || c.getPiece(sq("c1")) != 0 || c.getPiece(sq("b1")) != 0 {
+				return false
+			}
+			// WARNING: I THINK THERE IS NO OTHER EDGE CASE
+			return true
+		}
+		if (move.from == pair{col: 4, row: 0}) && (move.to == pair{col: 6, row: 0}) {
+			if !c.WhiteKingCastle {
+				return false
+			}
+			if c.SquareIsThreattenned(BLACK, sq("e1")) || c.SquareIsThreattenned(BLACK, sq("f1")) || c.SquareIsThreattenned(BLACK, sq("g1")) {
+				return false
+			}
+			if c.getPiece(sq("f1")) != 0 || c.getPiece(sq("g1")) != 0 {
+				return false
+			}
+			// WARNING: I THINK THERE IS NO OTHER EDGE CASE
+			return true
+		}
+	// en passant
+
+	// promotion
+
+	// if non special move
+	default:
+		c.erasePiece(move.from)
+		c.putPiece(move.to, fromPiece)
+	}
+
+	// check legality of c.BoardState
+
+	// restore c.BoardState
+	for _, square := range squaresToErase {
+		c.erasePiece(square)
+	}
+
+	for i := 0; i < len(piecesPerSquare); i++ {
+		c.putPiece(squaresToPutPieces[i], piecesPerSquare[i])
+	}
+
 	return true
 }
 
 func (c *Chessgame) getPiece(square pair) int {
+	for i := 0; i < len(c.BoardState); i++ {
+		if c.BoardState[i]&uint64(1<<pairToInt(square)) == 1 {
+			return i
+		}
+	}
 	return 0
 }
 
